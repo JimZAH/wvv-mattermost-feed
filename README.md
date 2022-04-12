7aeb3696fcda5d2463cd52482e877ad43ee58da5
JimZAH <james@colderwood.com>
2022-04-12 20:48:11 +0100

    First commit, sample bot

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wvv-mattermost-feed
 A bot that sends public messages to the mx0wvv.org website
+
+Current code is based on the example provided by mattermost
---
+++ main.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"regexp"
+	"strings"
+
+	"github.com/mattermost/mattermost-server/model"
+)
+
+const (
+	NAME             = "Website bot"
+	TEAM_NAME        = "teamwave"
+	CHANNEL          = "Town Square"
+	CHANNEL_LOG_NAME = "debugging-for-mx0wvv-feed"
+)
+
+var (
+	token    = os.Getenv("wvvusername")
+	password = os.Getenv("wvvpassword")
+)
+
+var client *model.Client4
+var webSocketClient *model.WebSocketClient
+
+var botUser *model.User
+var botTeam *model.Team
+var debuggingChannel *model.Channel
+
+func main() {
+
+	// Check token and passwords set
+	if token == "" || password == "" {
+		log.Fatal("Token or password are unset!")
+	}
+
+	SetupGracefulShutdown()
+
+	client = model.NewAPIv4Client("https://letstalkradio.org")
+
+	MakeSureServerIsRunning()
+
+	LoginAsTheBotUser()
+
+	FindBotTeam()
+
+	CreateBotDebuggingChannelIfNeeded()
+
+	SendMsgToDebuggingChannel("_"+NAME+" has **started** running_", "")
+
+	webSocketClient, err := model.NewWebSocketClient4("wss://letstalkradio.org", client.AuthToken)
+	if err != nil {
+		println("We failed to connect to the web socket")
+		PrintError(err)
+	}
+
+	webSocketClient.Listen()
+
+	go func() {
+		for resp := range webSocketClient.EventChannel {
+			HandleWebSocketResponse(resp)
+		}
+	}()
+
+	// You can block forever with
+	select {}
+
+}
+
+func CreateBotDebuggingChannelIfNeeded() {
+	if rchannel, resp := client.GetChannelByName(CHANNEL_LOG_NAME, botTeam.Id, ""); resp.Error != nil {
+		println("We failed to get the channels")
+		PrintError(resp.Error)
+	} else {
+		debuggingChannel = rchannel
+		return
+	}
+
+	// Looks like we need to create the logging channel
+	channel := &model.Channel{}
+	channel.Name = CHANNEL_LOG_NAME
+	channel.DisplayName = "Debugging For Sample Bot"
+	channel.Purpose = "This is used as a test channel for logging bot debug messages"
+	channel.Type = model.CHANNEL_OPEN
+	channel.TeamId = botTeam.Id
+	if rchannel, resp := client.CreateChannel(channel); resp.Error != nil {
+		println("We failed to create the channel " + CHANNEL_LOG_NAME)
+		PrintError(resp.Error)
+	} else {
+		debuggingChannel = rchannel
+		println("Looks like this might be the first run so we've created the channel " + CHANNEL_LOG_NAME)
+	}
+}
+
+func FindBotTeam() {
+	if team, resp := client.GetTeamByName(TEAM_NAME, ""); resp.Error != nil {
+		println("We failed to get the initial load")
+		println("or we do not appear to be a member of the team '" + TEAM_NAME + "'")
+		PrintError(resp.Error)
+		os.Exit(1)
+	} else {
+		botTeam = team
+	}
+}
+
+func HandleWebSocketResponse(event *model.WebSocketEvent) {
+	HandleMsgFromDebuggingChannel(event)
+}
+
+func HandleMsgFromDebuggingChannel(event *model.WebSocketEvent) {
+	// If this isn't the debugging channel then lets ingore it
+	if event.Broadcast.ChannelId != debuggingChannel.Id {
+		return
+	}
+
+	// Lets only reponded to messaged posted events
+	if event.Event != model.WEBSOCKET_EVENT_POSTED {
+		return
+	}
+
+	println("responding to debugging channel msg")
+
+	post := model.PostFromJson(strings.NewReader(event.Data["post"].(string)))
+	if post != nil {
+
+		// ignore my events
+		if post.UserId == botUser.Id {
+			return
+		}
+
+		// if you see any word matching 'alive' then respond
+		if matched, _ := regexp.MatchString(`(?:^|\W)alive(?:$|\W)`, post.Message); matched {
+			SendMsgToDebuggingChannel("Yes I'm running", post.Id)
+			return
+		}
+
+		// if you see any word matching 'up' then respond
+		if matched, _ := regexp.MatchString(`(?:^|\W)up(?:$|\W)`, post.Message); matched {
+			SendMsgToDebuggingChannel("Yes I'm running", post.Id)
+			return
+		}
+
+		// if you see any word matching 'running' then respond
+		if matched, _ := regexp.MatchString(`(?:^|\W)running(?:$|\W)`, post.Message); matched {
+			SendMsgToDebuggingChannel("Yes I'm running", post.Id)
+			return
+		}
+
+		// if you see any word matching 'hello' then respond
+		if matched, _ := regexp.MatchString(`(?:^|\W)hello(?:$|\W)`, post.Message); matched {
+			SendMsgToDebuggingChannel("Yes I'm running", post.Id)
+			return
+		}
+	}
+
+	SendMsgToDebuggingChannel("I did not understand you!", post.Id)
+}
+
+func LoginAsTheBotUser() {
+	if user, resp := client.Login(token, password); resp.Error != nil {
+		println("There was a problem logging into the Mattermost server.  Are you sure ran the setup steps from the README.md?")
+		PrintError(resp.Error)
+		os.Exit(1)
+	} else {
+		botUser = user
+	}
+}
+
+func MakeSureServerIsRunning() {
+	if props, resp := client.GetOldClientConfig(""); resp.Error != nil {
+		println("There was a problem pinging the Mattermost server.  Are you sure it's running?")
+		PrintError(resp.Error)
+		os.Exit(1)
+	} else {
+		println("Server detected and is running version " + props["Version"])
+	}
+}
+
+func PrintError(err *model.AppError) {
+	println("\tError Details:")
+	println("\t\t" + err.Message)
+	println("\t\t" + err.Id)
+	println("\t\t" + err.DetailedError)
+}
+
+func SetupGracefulShutdown() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for _ = range c {
+			if webSocketClient != nil {
+				webSocketClient.Close()
+			}
+
+			SendMsgToDebuggingChannel("_"+NAME+" has **stopped** running_", "")
+			os.Exit(0)
+		}
+	}()
+}
+
+func SendMsgToDebuggingChannel(msg string, replyToId string) {
+	post := &model.Post{}
+	post.ChannelId = debuggingChannel.Id
+	post.Message = msg
+
+	post.RootId = replyToId
+
+	if _, resp := client.CreatePost(post); resp.Error != nil {
+		println("We failed to send a message to the logging channel")
+		PrintError(resp.Error)
+	}
+}
